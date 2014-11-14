60da9ca0d3b57735c37c7546d70daa07a40c9f64
Alex Wissmann <awiss@wharton.upenn.edu>
2014-11-13 22:17:16 -0500

    Moved transit on to wrapper-base

--- penn/transit.py
+++ penn/transit.py
@@ -1,7 +1,7 @@
 """A module for consuming the Penn Registrar API"""
 from os import path
 import requests
-
+from base import WrapperBase
 
 BASE_URL = "https://esb.isc-seo.upenn.edu/8091/open_data/transit/"
 ENDPOINTS = {
@@ -12,39 +12,17 @@ ENDPOINTS = {
     'STOPTIMES': BASE_URL + 'stoptimes'
 }
 
-class Transit(object):
-    """The client for the Registrar. Used to make requests to the API.
+class Transit(WrapperBase):
+    """The client for Transit. Used to make requests to the API.
 
     :param bearer: The user code for the API
     :param token: The password code for the API
 
     Usage::
 
-      >>> from penn.directory import Directory
-      >>> din = Dining('MY_USERNAME_TOKEN', 'MY_PASSWORD_TOKEN')
+      >>> from penn.transit import Transit
+      >>> trans = Transit('MY_USERNAME_TOKEN', 'MY_PASSWORD_TOKEN')
     """
-    def __init__(self, bearer, token):
-        self.bearer = bearer
-        self.token = token
-
-    @property
-    def headers(self):
-        """The HTTP headers needed for signed requests"""
-        return {
-            "Authorization-Bearer": self.bearer,
-            "Authorization-Token": self.token,
-        }
-
-    def _request(self, url, params=None):
-        """Make a signed request to the API, raise any API errors, and returning a tuple
-        of (data, metadata)"""
-        response = requests.get(url, params=params, headers=self.headers).json()
-
-        if response['service_meta']['error_text']:
-            raise ValueError(response['service_meta']['error_text'])
-
-        return response
-
 
     def formatDate(self, date):
         #print date.strftime("%d/%m/%Y")+ "%20" + date.strftime("%H24:%M:%S")
