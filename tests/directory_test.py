879746b9dd399c0c5a2d086e527694b9045955c9
Adel Qalieh <aqalieh95@gmail.com>
2016-09-20 11:32:23 -0400

    Fix directory test, Alex no longer a student

--- tests/directory_test.py
+++ tests/directory_test.py
@@ -12,6 +12,6 @@ class TestDirectory(unittest.TestCase):
         self.dir = directory.Directory(username, password)
 
     def test_lastname_search(self):
-        person = self.dir.detail_search({'last_name':'Wissmann'})['result_data']
+        person = self.dir.detail_search({'last_name':'Domingoes'})['result_data']
         self.assertEquals(len(person), 1)
-        self.assertEquals(person[0]['result_data'][0]['detail_name'], "ALEXANDER R WISSMANN")
+        self.assertEquals(person[0]['result_data'][0]['detail_name'], "ADAM W DOMINGOES")
