74d5a6e11cbcbc95a42657adbad76d0451858dbd
Adel Qalieh <aqalieh95@gmail.com>
2016-10-28 20:39:09 -0400

    Update version number to 1.3.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     url='https://github.com/pennlabs/penn-sdk-python',
     author='Penn Labs',
     author_email='pennappslabs@gmail.com',
-    version='1.2.1',
+    version='1.3.0',
     packages=['penn'],
     license='MIT',
     package_data={
