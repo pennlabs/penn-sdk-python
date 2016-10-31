044977c6607958abcdd2ed0aa7900ca7d902840e
esqu1 <brandbest1@gmail.com>
2016-10-31 14:48:26 -0400

    v1.4.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     url='https://github.com/pennlabs/penn-sdk-python',
     author='Penn Labs',
     author_email='admin@pennlabs.org',
-    version='1.3.2',
+    version='1.4.0',
     packages=['penn'],
     license='MIT',
     package_data={
