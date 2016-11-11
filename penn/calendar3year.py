6b52e5b142df6ad25236ad26177ab191f4aecc21
esqu1 <brandbest1@gmail.com>
2016-11-11 18:50:26 -0500

    year change added, code cleanup coming

--- penn/calendar3year.py
+++ penn/calendar3year.py
@@ -10,30 +10,17 @@ class Calendar(object):
     def __init__(self):
         pass
 
-    def pull(self):
-        '''Returns a list containing all the events from the 3 year calendar.
-
-        Each element of the list is formatted as follows:
-        [ <event name>, <year 1 date range>, <year 2 date range>, <year 3 date range> ]
-        '''
-        l = []
-        soup = BeautifulSoup(requests.get(BASE_URL).text, 'html5lib')
-        title = soup.find_all('p', class_='h2')[0].get_text(strip=True)
-        raw_calendar = soup.find_all('tbody')[16]
-        events = raw_calendar.find_all('tr')[1:-1]
-        for event in events:
-            if str(event['class'][0]) == 'bodytext':
-                dates = event.find_all('td')
-                dates_across_years = []
-                key = list(dates[0].descendants)[-1].encode('utf-8').strip()
-                for date in dates[2:]:
-                    value = list(date.descendants)[-1].encode('utf-8').strip()
-                    dates_across_years.append(value)
-                l.append([key] + list(dates_across_years))
-        return l
 
     @staticmethod
-    def range_parse(self, ran):
+    def title_parse(title):
+        '''Parses the title of the calendar to determine
+        the current year range.'''
+
+        ranges = title.split('-')
+        return [int(ranges[3]) - 3, int(ranges[3])]
+
+    @staticmethod
+    def range_parse(ran):
         '''Given a date range, returns a start and end date object
         from the datetime module.
 
@@ -56,10 +43,31 @@ class Calendar(object):
             end_date = datetime.date(1900, month, int(end))
         return [start_date, end_date]
 
-    @staticmethod
-    def title_parse(self, title):
-        '''Parses the title of the calendar to determine
-        the current year range.'''
+    def pull(self):
+        '''Returns a list containing all the events from the 3 year calendar.
 
-        ranges = title.split('-')
-        return [int(ranges[0]), int(ranges[2])]
+        Each element of the list is formatted as follows:
+        [ <event name>, <year 1 date range>, <year 2 date range>, <year 3 date range> ]
+        '''
+        l = []
+        year_change = 0
+        soup = BeautifulSoup(requests.get(BASE_URL).text, 'html5lib')
+        title = soup.find_all('p', class_='h2')[0].get_text(strip=True)
+        year_range = self.title_parse(title)
+        raw_calendar = soup.find_all('tbody')[16]
+        events = raw_calendar.find_all('tr')[2:-1]
+        for event in events:
+            if str(event['class'][0]) == 'bodytext':
+                dates = event.find_all('td')
+                dates_across_years = []
+                key = list(dates[0].descendants)[-1].encode('utf-8').strip()
+                for date in dates[2:]:
+                    value = list(date.descendants)[-1].encode('utf-8').strip()
+                    dates_across_years.append(value)
+                l.append([key] + list(dates_across_years) + [year_range[0] + year_change])
+            elif year_change != 1 and str(event['class'][0]) == 'rightSideLinkHeadings':
+                year_change = 1
+        return l
+
+
+
