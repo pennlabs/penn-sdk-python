e1451184f160100fdb09b71efabaf4c606aa905e
esqu1 <brandbest1@gmail.com>
2016-11-11 17:51:55 -0500

    pull method finished

--- penn/calendar.py
+++ penn/calendar.py
@@ -1,28 +1,36 @@
 from bs4 import BeautifulSoup
 import requests
+import datetime
 
 
-BASE_URL = "https://mobile.apps.upenn.edu/mobile/jsp/fast.do?fastStart=academicCalendarPage"
+BASE_URL = "http://www.upenn.edu/almanac/3yearcal.html"
 
 
 class Calendar(object):
     def __init__(self):
         pass
 
-    def pull(self):
-        d = {}
+    def pull():
+        '''Returns a list containing all the events from the 3 year calendar.
+
+        Each element of the list is formatted as follows:
+        [ <event name>, <year 1 date range>, <year 2 date range>, <year 3 date range> ]
+        '''
+        l = []
         soup = BeautifulSoup(requests.get(BASE_URL).text, 'html5lib')
-        raw_calendar = soup.find_all('table', border='1')[0]
-        soup = BeautifulSoup(raw_calendar, 'html5lib')
-        events = soup.find_all('tr')[2:-1]
+        raw_calendar = soup.find_all('tbody')[16]
+        events = raw_calendar.find_all('tr')[1:-1]
         for event in events:
-            soup = BeautifulSoup(event, 'html5lib')
-            dates = soup.find_all('td')
-            dates_across_years = []
-            key = dates[0][5:-5]
-            key = BeautifulSoup(key, 'html5lib').a.content
-            for date in dates[1:]:
-                value = BeautifulSoup(date[5:-5], 'html5lib').a.content
-                dates_across_years.append(value)
-            d[key] = dates_across_years
-        return d
+            if str(event['class'][0]) == 'bodytext':
+                dates = event.find_all('td')
+                dates_across_years = []
+                key = list(dates[0].descendants)[-1].encode('utf-8').strip()
+                for date in dates[2:]:
+                    value = list(date.descendants)[-1].encode('utf-8').strip()
+                    print value
+                    dates_across_years.append(value)
+                l.append([key] + list(dates_across_years))
+        return l
+
+
+
