263b21cf2535be4df8e8fd511e30017ec10e7b2d
esqu1 <brandbest1@gmail.com>
2016-11-12 12:21:57 -0500

    tests added

--- tests/calendar_test.py
+++ tests/calendar_test.py
@@ -10,8 +10,13 @@ class TestCalendar():
     def test_pull(self):
         l = self.calendar.pull_3year()
         ok_(len(l) > 0)
-        for event in l[0: 5]:
+        for event in l:
             ok_(len(event) == 5)
             d = Calendar.range_parse(event[1], event[4])
             change = (d[1] - d[0]).total_seconds()
             ok_(change >= 0)
+        years = []
+        for event in l:
+            if event[4]  not in years:
+                years.append(event[4])
+        ok_(len(years) == 2)
