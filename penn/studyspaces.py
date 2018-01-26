b20f35f0fd91497c15c9688c12a752945bf602e6
Eric Wang <ezwang1@gmail.com>
2018-01-25 21:18:25 -0500

    add room and thumbnail info

--- penn/studyspaces.py
+++ penn/studyspaces.py
@@ -2,6 +2,8 @@ import requests
 import datetime
 import json
 import pytz
+import re
+import six
 
 from bs4 import BeautifulSoup
 
@@ -25,9 +27,23 @@ class StudySpaces(object):
         date = pytz.timezone("US/Eastern").localize(datetime.datetime.strptime(date, "%Y-%m-%d %H:%M:%S"))
         return date.isoformat()
 
+    @staticmethod
+    def get_room_id_name_mapping(building):
+        data = requests.get("{}/spaces?lid={}".format(BASE_URL, building)).content.decode("utf8")
+        # find all of the javascript room definitions
+        out = {}
+        for item in re.findall(r"resources.push\(((?s).*?)\);", data, re.MULTILINE):
+            items = {k: v for k, v in re.findall(r'(\w+?):\s*(.*?),', item)}
+            out[int(items["eid"])] = {
+                "name": items["title"][1:-1].encode().decode("unicode_escape" if six.PY3 else "string_escape"),
+                "thumbnail": items["thumbnail"][1:-1]
+            }
+        return out
+
     def get_rooms(self, building, start, end):
         """Returns a dictionary matching all rooms given a building id and a date range."""
 
+        mapping = self.get_room_id_name_mapping(building)
         room_endpoint = "{}/process_equip_p_availability.php".format(BASE_URL)
         data = {
             "lid": building,
@@ -47,4 +63,9 @@ class StudySpaces(object):
                 "end": self.format_date(row["end"]),
                 "booked": row["status"] != 0
             })
-        return [{"room_id": k, "times": v} for k, v in rooms.items()]
+        return [{
+            "room_id": k,
+            "times": v,
+            "name": mapping[k]["name"] if k in mapping else None,
+            "thumbnail": mapping[k]["thumbnail"] if k in mapping else None
+        } for k, v in rooms.items()]
