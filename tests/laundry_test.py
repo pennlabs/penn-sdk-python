cdfce9aaa56470cd5a1c61daa059a53c638be9d6
Eric Wang <ezwang1@gmail.com>
2018-01-27 11:43:36 -0500

    #75, fix laundry tests

--- tests/laundry_test.py
+++ tests/laundry_test.py
@@ -1,43 +1,38 @@
-from nose.tools import ok_, eq_
+from nose.tools import ok_
 from penn import Laundry
 
 
 class TestLaundry():
     def setUp(self):
-        pass
-        # self.laundry = Laundry()
+        self.laundry = Laundry()
 
     def test_all(self):
-        pass
-        # data = self.laundry.all_status()
-        # ok_(len(data) > 50)
-        # eq_('Class of 1925 House', data[0]['name'])
-        # # Check all halls have appropriate data points
-        # for i, hall in enumerate(data):
-        #     # Not a valid check anymore because of New College House
-        #     # eq_(hall['hall_no'], i)
-        #     ok_(hall['dryers_available'] >= 0)
-        #     ok_(hall['dryers_in_use'] >= 0)
-        #     ok_(hall['washers_available'] >= 0)
-        #     ok_(hall['washers_in_use'] >= 0)
+        data = self.laundry.all_status()
+        ok_(len(data) > 50)
+        ok_('Class of 1925' in data)
+        # Check all halls have appropriate data points
+        for i, hall in data.items():
+            for t in ['washers', 'dryers']:
+                ok_("running" in hall[t])
+                ok_("open" in hall[t])
 
     def test_single_hall(self):
-        pass
-        # for i in range(1):
-        #     data = self.laundry.hall_status(i)
-        #     machines = data['machines']
-        #     # Check all machines have appropriate data points
-        #     for i, machine in enumerate(machines):
-        #         eq_(machine['number'], str(i + 1))
-        #         ok_('available' in machine)
-        #         ok_('machine_type' in machine)
-        #         ok_('time_left' in machine)
+        for i in range(3):
+            data = self.laundry.hall_status(i)
+            machines = data['machines']
+            # Check for general hall information
+            ok_('washers' in machines)
+            ok_('dryers' in machines)
+            # Check all machines have appropriate data points
+            for machine in machines["details"]:
+                ok_('id' in machine)
+                ok_('type' in machine)
+                ok_('status' in machine)
 
     def test_usage(self):
-        pass
-        # for i in range(10):
-        #     data = self.laundry.machine_usage(i)
-        #     for j in data:
-        #         ok_(j in self.laundry.days)
-        #         for k in data[j]:
-        #             ok_(k in self.laundry.busy_dict.values())
+        for i in range(10):
+            data = self.laundry.machine_usage(i)
+            for j in data:
+                ok_(j in self.laundry.days)
+                for k in data[j]:
+                    ok_(k in self.laundry.busy_dict.values())
