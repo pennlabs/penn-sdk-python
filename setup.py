b1dec3267f53d428d56a633dd61e14c9ba9d85cb
Eric Wang <ezwang1@gmail.com>
2018-03-08 14:29:01 -0500

    set version to 1.7.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     url='https://github.com/pennlabs/penn-sdk-python',
     author='Penn Labs',
     author_email='admin@pennlabs.org',
-    version='1.6.9',
+    version='1.7.0',
     packages=['penn'],
     license='MIT',
     package_data={
