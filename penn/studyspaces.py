f3e71074b2ef1a2d75e423b450955c7448ef1d2d
Eric Wang <ezwang1@gmail.com>
2018-04-10 16:15:41 -0400

    strip html from error response

--- penn/studyspaces.py
+++ penn/studyspaces.py
@@ -208,7 +208,7 @@ class StudySpaces(object):
         resp = self._request("POST", "/1.1/space/reserve", json=data)
         out = resp.json()
         if "errors" in out and "error" not in out:
-            out["error"] = out["errors"]
+            out["error"] = BeautifulSoup(out["errors"].replace("\n", " "), "html.parser").text.strip()
             del out["errors"]
         if "results" not in out:
             if "error" not in out:
