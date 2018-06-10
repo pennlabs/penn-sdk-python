09b28f47c4b8642992402293ade4370c220b05a6
Eric Wang <ezwang1@gmail.com>
2018-06-10 14:22:53 -0400

    remove old/broken libraries

--- penn/__init__.py
+++ penn/__init__.py
@@ -4,10 +4,8 @@ from .registrar import Registrar
 from .directory import Directory
 from .dining import Dining
 from .dining import DiningV2
-from .transit import Transit
 from .news import News
 from .laundry import Laundry
-from .map import Map
 from .studyspaces import StudySpaces
 from .calendar3year import Calendar
 from .fitness import Fitness
