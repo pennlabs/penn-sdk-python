c4c603a69d63ef3cc0a09f4efea93b6da71f292d
Eric Wang <ezwang1@gmail.com>
2018-07-02 19:03:23 -0400

    bump version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     url='https://github.com/pennlabs/penn-sdk-python',
     author='Penn Labs',
     author_email='admin@pennlabs.org',
-    version='1.7.0',
+    version='1.7.1',
     packages=['penn'],
     license='MIT',
     package_data={
