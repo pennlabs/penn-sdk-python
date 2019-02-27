8ca7dc089df7981fa8cdaed24a002c6e5e435f23
Josh <jsdoman97@gmail.com>
2019-02-27 12:21:10 -0500

    updated version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     url='https://github.com/pennlabs/penn-sdk-python',
     author='Penn Labs',
     author_email='admin@pennlabs.org',
-    version='1.8.1',
+    version='1.8.2',
     packages=['penn'],
     license='MIT',
     package_data={
