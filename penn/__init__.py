788486107e4ce8b83f0c88f78a40491325ad248c
Josh <jsdoman97@gmail.com>
2019-11-03 11:43:44 -0500

    updated version

--- penn/__init__.py
+++ penn/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '1.8.5'
+__version__ = '1.8.6'
 
 from .registrar import Registrar
 from .directory import Directory
--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     url='https://github.com/pennlabs/penn-sdk-python',
     author='Penn Labs',
     author_email='admin@pennlabs.org',
-    version='1.8.5',
+    version='1.8.6',
     packages=['penn'],
     license='MIT',
     package_data={
